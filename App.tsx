d049f0be966e53b74feb87ca7b5cae818c25b2c6
RobertoSaucedoL <zircksaucedo@gmail.com>
2025-11-26 12:31:29 -0600

    App.tsx

--- App.tsx
+++ App.tsx
@@ -24,7 +24,7 @@ const INITIAL_LOANS: LoanParams[] = [
   },
   { 
     id: '2', 
-    name: 'REVOLVENTE BANAMEX', 
+    name: 'Revolvente Banamex', 
     principal: 6527242, 
     annualRate: 13.87, 
     years: 3, 
